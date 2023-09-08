07d31d83bdec656f818aad6812e03bb6f4cbe571
Connor Lennox <clennox99@gmail.com>
2023-09-08 14:50:39 -0400

    bgm plays

---
+++ nodes/bgm.gd
@@ -0,0 +1,24 @@
+extends AudioStreamPlayer
+
+const TITLE_TRACK = "title"
+const GAME_TRACK = "game"
+
+var current_track = ""
+
+
+func _ready():
+	volume_db = -5
+
+
+func play_title_music():
+	if current_track != TITLE_TRACK:
+		stream = load("res://assets/music/title_music.ogg")
+		play()
+		current_track = TITLE_TRACK
+
+
+func play_game_music():
+	if current_track != GAME_TRACK:
+		stream = load("res://assets/music/game_music.ogg")
+		play()
+		current_track = GAME_TRACK
--- scenes/player_test_scene.gd
+++ scenes/player_test_scene.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
+	Bgm.play_game_music()
+	
 	$TestItem2.food_type = load("res://items/resources/lettuce_whole_item.tres")
 	$TestItem4.food_type = load("res://items/resources/tomato_whole_item.tres")
 	$PlateItem2.dirty = false
