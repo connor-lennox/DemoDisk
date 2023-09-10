453a0a41c5ef94071e29629a960d5bbd5a6b3001
Connor Lennox <clennox99@gmail.com>
2023-09-10 19:21:48 -0400

    more interesting title screen

--- scenes/title/title_scene.gd
+++ scenes/title/title_scene.gd
@@ -1,9 +1,12 @@
 extends Node
 
+@onready var camera_pivot = $CameraPivot
 
 func _ready():
 	Bgm.play_title_music()
 
+func _process(delta):
+	camera_pivot.global_rotation.y += delta * 0.2
 
 func _start_game():
 	SceneManager.switch_to_game()
