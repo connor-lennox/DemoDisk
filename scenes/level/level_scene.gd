8527e3a60105259ace34e57d1eae269e10835a1a
Connor Lennox <clennox99@gmail.com>
2023-09-12 20:51:54 -0400

    customers spawn regularly

--- scenes/level/level_scene.gd
+++ scenes/level/level_scene.gd
@@ -3,19 +3,23 @@ extends Node3D
 const PLATE_RETURN_TIME = 10
 
 @onready var customer_manager: CustomerManager = $CustomerManager
+@onready var customer_spawn_timer = $CustomerSpawnTimer
 
 @onready var sink = $Restaurant/Stations/SinkStation/Sink
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Bgm.play_game_music()
-	customer_manager.spawn_customer()
-	customer_manager.spawn_customer()
+	_spawn_customer()
 
+func _spawn_customer():
+	if len(customer_manager.customers) <= 6:
+		customer_manager.spawn_customer()
+	customer_spawn_timer.wait_time = randf_range(15, 20)
+	customer_spawn_timer.start()
 
 func _order_completed():
 	_wait_and_return_plate()
-
 
 func _wait_and_return_plate():
 	await get_tree().create_timer(PLATE_RETURN_TIME).timeout
